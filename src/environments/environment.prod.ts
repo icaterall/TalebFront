9d62d1cc8cf1ed446fdcb683a8d5123baaa6c698
icaterall <ashqahman@gmail.com>
2025-09-20 13:48:49 +0400

    yes

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: true,
+  hmr: false,
+  frontendUrl:"https://anataleb.com",
+  apiUrl:"https://backend.anataleb.com/api/v1",
+  apiSocketUrl:"https://backend.anataleb.com/",
+  apiDirectUrl:"https://backend.anataleb.com/",
+  apiUploadUrl:"https://anataleb.s3.us-east-2.amazonaws.com/",
+  stripe: {
+    publicKey: 'pk_test_S1eEGdRF5Itmnfwk9rNHFZnV00YlGgdUtt',
+  }
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: false,
+  hmr: false, 
+  frontendUrl:"http://localhost:4200",
+  apiUrl:"http://127.0.0.1:3000/api/v1",
+  apiDirectUrl:"http://127.0.0.1:3000/",
+  apiUploadUrl:"https://pickpickgo.s3.ap-southeast-1.amazonaws.com",
+   apiSocketUrl:"http://127.0.0.1:3000/",
+   stripe: {
+      publicKey: 'pk_test_S1eEGdRF5Itmnfwk9rNHFZnV00YlGgdUtt',
+    }
+};
