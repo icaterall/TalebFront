29d8351092892307007edbffcd1c0168564bed85
icaterall <57697382+icaterall@users.noreply.github.com>
2025-09-22 15:14:34 +0400

    taleb

--- src/app/shared/footer/footer.component.ts
+++ src/app/shared/footer/footer.component.ts
@@ -1,12 +1,25 @@
-import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { RouterLink } from '@angular/router';
 import { TranslateModule } from '@ngx-translate/core';
+
+
 
 @Component({
   selector: 'app-footer',
-  standalone: true,
-  imports: [CommonModule, TranslateModule],
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.scss'],
+  standalone: true,
+  imports: [CommonModule, RouterLink, TranslateModule]
+
 })
-export class FooterComponent {}
+export class FooterComponent implements OnInit {
+  constructor() { }
+  year: number = new Date().getFullYear();
+  ngOnInit(): void {
+  }
+  scrollToTop(): void {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+  }
+  
+}
