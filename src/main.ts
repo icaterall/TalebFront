18bbfeccaec53dd2a691ae95e024d838f10a174b
icaterall <57697382+icaterall@users.noreply.github.com>
2025-09-24 15:43:42 +0400

    ok

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { App } from './app/app';
+console.log('🚀 Bootstrapping Angular app...');
 
 bootstrapApplication(App, appConfig).then(() => {
   window.dispatchEvent(new Event('AnatalebReady'));
